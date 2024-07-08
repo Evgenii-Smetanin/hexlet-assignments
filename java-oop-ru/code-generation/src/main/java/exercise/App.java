08894c5b5cfec3dc9d6cca19ed218cbfc8d241e3
@hexlet/cli <support@hexlet.io>
2024-07-08 19:50:08 +0300

    submit java-oop-ru/code-generation

---
+++ java-oop-ru/code-generation/src/main/java/exercise/App.java
@@ -0,0 +1,16 @@
+package exercise;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+// BEGIN
+public class App {
+    public static void save(Path path, Car car) throws Exception {
+        Files.writeString(path, car.serialize());
+    }
+
+    public static Car extract(Path path) throws IOException {
+        return Car.unserialize(Files.readString(path));
+    }
+}
+// END
---
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -0,0 +1,27 @@
+package exercise;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Value;
+
+// BEGIN
+@Value
+// END
+class Car {
+    int id;
+    String brand;
+    String model;
+    String color;
+    User owner;
+
+    public static Car unserialize(String carString) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(carString, Car.class);
+    }
+
+    // BEGIN
+    public String serialize() throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(this);
+    }
+    // END
+}
