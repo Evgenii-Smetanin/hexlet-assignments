463290a0cfb7dfb85bdd6440d6733971c7b78961
@hexlet/cli <support@hexlet.io>
2024-07-08 19:54:58 +0300

    submit java-oop-ru/code-generation

--- java-oop-ru/code-generation/src/main/java/exercise/Car.java
+++ java-oop-ru/code-generation/src/main/java/exercise/Car.java
@@ -1,7 +1,10 @@
 package exercise;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Value;
+
+import java.io.IOException;
 
 // BEGIN
 @Value
